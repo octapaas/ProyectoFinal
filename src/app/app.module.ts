01faef8e95d73fb5ff38a268c2ab44349329c31c
Octavio Cikos <octavio.cikos@gmail.com>
2022-05-05 23:22:37 -0300

    Banner, Header y otras cosas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgentinaProgramaComponent } from './components/logo-argentina-programa/logo-argentina-programa.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgentinaProgramaComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/logo-argentina-programa/logo-argentina-programa.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoArgentinaProgramaComponent } from './logo-argentina-programa.component';
+
+describe('LogoArgentinaProgramaComponent', () => {
+  let component: LogoArgentinaProgramaComponent;
+  let fixture: ComponentFixture<LogoArgentinaProgramaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoArgentinaProgramaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoArgentinaProgramaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
